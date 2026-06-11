40a47372bb15bcbedc36e65f89dbd78fe5eeb178
godreyleo-ops <godreyleo@gmail.com>
2026-06-12 02:08:47 +0300

    config: Add PostgreSQL database connection module

---
+++ config/database.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  connect: async () => {
+    const client = await pool.connect();
+    client.release();
+    return true;
+  },
+  pool
+};
